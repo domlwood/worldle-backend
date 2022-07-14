38e08b4e043ee5546b7a227a763af797998c734f
Dominic Wood <dominic.wood@theapsgroup.com>
2022-07-14 19:11:58 +0100

    feat: add authentication

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const app = require("express")();
 const haversine = require("haversine");
+require('dotenv').config()
 
+const API_KEY = process.env.API_KEY;
 const PORT = process.env.PORT || 3000;
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "https://worldle-dwd.netlify.app");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://worldle-dwd.netlify.app"
+  );
   res.setHeader("Access-Control-Allow-Methods", "GET");
   res.setHeader(
     "Access-Control-Allow-Headers",
@@ -14,60 +19,63 @@ app.use(function (req, res, next) {
 });
 
 app.get("/", () => {
-  res.send("successful setup")
-})
+  res.send("successful setup");
+});
 
 app.get("/distance", (req, res) => {
-  params = JSON.parse(req.query.distance);
-
-  function toRadians(degrees) {
-    return (degrees * Math.PI) / 180;
+  if (req.headers.authorization !== API_KEY) {
+    res.status(401).send("unauthorized access");
   }
-  function toDegrees(radians) {
-    return (radians * 180) / Math.PI;
-  }
-  function calcBearing(startLat, startLng, destLat, destLng) {
-    startLat = toRadians(startLat);
-    startLng = toRadians(startLng);
-    destLat = toRadians(destLat);
-    destLng = toRadians(destLng);
+    params = JSON.parse(req.query.distance);
 
-    y = Math.sin(destLng - startLng) * Math.cos(destLat);
-    x =
-      Math.cos(startLat) * Math.sin(destLat) -
-      Math.sin(startLat) * Math.cos(destLat) * Math.cos(destLng - startLng);
-    brng = Math.atan2(y, x);
-    brng = toDegrees(brng);
-    return (brng + 360) % 360;
-  }
+    function toRadians(degrees) {
+      return (degrees * Math.PI) / 180;
+    }
+    function toDegrees(radians) {
+      return (radians * 180) / Math.PI;
+    }
+    function calcBearing(startLat, startLng, destLat, destLng) {
+      startLat = toRadians(startLat);
+      startLng = toRadians(startLng);
+      destLat = toRadians(destLat);
+      destLng = toRadians(destLng);
 
-  const start = {
-    latitude: params[0].latitude,
-    longitude: params[0].longitude,
-  };
-  const end = {
-    latitude: params[1].latitude,
-    longitude: params[1].longitude,
-  };
-  const distance = haversine(start, end);
-  console.log(
-    params[0].latitude,
-    params[0].longitude,
-    params[1].latitude,
-    params[1].longitude
-  );
+      y = Math.sin(destLng - startLng) * Math.cos(destLat);
+      x =
+        Math.cos(startLat) * Math.sin(destLat) -
+        Math.sin(startLat) * Math.cos(destLat) * Math.cos(destLng - startLng);
+      brng = Math.atan2(y, x);
+      brng = toDegrees(brng);
+      return (brng + 360) % 360;
+    }
 
-  const bearing = calcBearing(
-    params[0].latitude,
-    params[0].longitude,
-    params[1].latitude,
-    params[1].longitude
-  );
+    const start = {
+      latitude: params[0].latitude,
+      longitude: params[0].longitude,
+    };
+    const end = {
+      latitude: params[1].latitude,
+      longitude: params[1].longitude,
+    };
+    const distance = haversine(start, end);
+    console.log(
+      params[0].latitude,
+      params[0].longitude,
+      params[1].latitude,
+      params[1].longitude
+    );
 
-  res.status(200).send({
-    distance: distance,
-    bearing: bearing,
-  });
+    const bearing = calcBearing(
+      params[0].latitude,
+      params[0].longitude,
+      params[1].latitude,
+      params[1].longitude
+    );
+
+    res.status(200).send({
+      distance: distance,
+      bearing: bearing,
+    });
 });
 
 app.listen(PORT, () => console.log(`it's alive on http://localhost:${PORT}`));
