15d6c96812cee2af3b809bb2071c5f8b09aaa6c2
Dominic Wood <dominic.wood@theapsgroup.com>
2022-07-14 19:15:51 +0100

    chore: ammend CORS headers

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Methods", "GET");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
+    "authorization"
   );
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
